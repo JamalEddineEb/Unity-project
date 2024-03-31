464ad6f2cc052cec9f141bc0894579983b80e371
JamalEddineEb <jamal-eddine.el-betioui@etu.unice.fr>
2024-03-31 11:23:52 +0200

    animate turtle

---
+++ Assets/Codes/TriggerManager.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TriggerManager : MonoBehaviour
+{
+   public List<Checkpoint> checkpoints;
+   
+   public UIManager uiManager;
+   public Canvas checkpointCanvas;
+   public Button closeButton;
+   public Animator turtleAnimator;
+   
+
+   void Start()
+   {
+        Debug.Log("Start");
+        ListenCheckpoints(true);
+        checkpointCanvas.enabled = false;
+        if (closeButton != null)
+        {
+            closeButton.onClick.AddListener(HideCanvas);
+        }
+        else
+        {
+            Debug.LogWarning("Close Button not assigned in TriggerManager!");
+        }
+   }
+   
+   void HideCanvas()
+   {
+       checkpointCanvas.enabled = false;
+   }
+
+   private void ListenCheckpoints(bool subscribe)
+   {
+
+       foreach (Checkpoint checkpoint in checkpoints)
+       {
+           if (subscribe) checkpoint.onCheckpointEnter.AddListener(CheckpointActivated);
+           else checkpoint.onCheckpointEnter.RemoveListener(CheckpointActivated);
+       }
+   }
+
+   public void CheckpointActivated(GameObject princesse, Checkpoint checkpoint)
+   {
+       if (checkpoints.Contains(checkpoint))
+       {
+            Debug.Log("checkpoint " + checkpoint.checkpointID);
+            checkpointCanvas.enabled = true;
+            uiManager.UpdateText("Checkpoint " + checkpoint.checkpointID);
+            if(checkpoint.checkpointID.Equals("turtle")){
+                turtleAnimator.SetBool("animate", true);
+                Debug.Log("turtle animated");
+            }
+            
+       }
+   }
+}
