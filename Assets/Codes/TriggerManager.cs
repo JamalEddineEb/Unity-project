dbd375db01155fe8a519d4054425e02493f0db4f
Saad Beidouri <saad.beidouri@etu.unice.fr>
2024-04-07 00:04:03 +0200

    planet 1 90%

--- Assets/Codes/TriggerManager.cs
+++ Assets/Codes/TriggerManager.cs
@@ -9,8 +9,10 @@ public class TriggerManager : MonoBehaviour
    public Canvas checkpointCanvas;
    public Button closeButton;
    public Animator turtleAnimator;
-   
    public Animator freeKickMachineAnimation;
+   public Animator rocketAnimator;
+   public Button raceButton;
+
    
    
 
@@ -28,6 +30,7 @@ public class TriggerManager : MonoBehaviour
         {
             Debug.LogWarning("Close Button not assigned in TriggerManager!");
         }
+        
    }
    
    void HideCanvas()
@@ -44,24 +47,53 @@ public class TriggerManager : MonoBehaviour
            else checkpoint.onCheckpointEnter.RemoveListener(CheckpointActivated);
        }
    }
+   
+   void CarButtonClicked()
+   {
+       // Add functionality for when the new button is clicked
+       Debug.Log("Car Button Clicked");
+       // You can add additional actions here if needed
+   }
 
    public void CheckpointActivated(GameObject princesse, Checkpoint checkpoint)
    {
+       raceButton.gameObject.SetActive(false);
        if (checkpoints.Contains(checkpoint))
        {
             Debug.Log("checkpoint " + checkpoint.checkpointID);
-            checkpointCanvas.enabled = true;
-            uiManager.UpdateText("Checkpoint " + checkpoint.checkpointID);
             if(checkpoint.checkpointID.Equals("turtle")){
                 turtleAnimator.SetBool("animate", true);
+                checkpointCanvas.enabled = true;
+                uiManager.UpdateText(
+                    "Turtles, gentle wanderers of the wild, embody the harmony of nature's rhythm. Their slow, deliberate movements echo the tranquil cadence of the natural world.\nJamal Eddine El Betoui");
                 Debug.Log("turtle animated");
             }
 
             if (checkpoint.checkpointID.Equals("freeKickMachine"))
             {
                 freeKickMachineAnimation.SetBool("animate", true);
+                checkpointCanvas.enabled = true;
+                uiManager.UpdateText(
+                    "The free-kick machine, a marvel of precision and power, channels the essence of football's artistry. With a single strike, it weaves moments of brilliance into the fabric of the game.\nSaad Beidouri");
                 Debug.Log("freeKickMachine animated");
                 
+            }
+
+            if (checkpoint.checkpointID.Equals("Rocket"))
+            {
+                rocketAnimator.SetBool("animate", true);
+                checkpointCanvas.enabled = true;
+                uiManager.UpdateText(
+                    "The rocket, a symbol of human ingenuity and ambition, soars through the heavens with grace and power.\nLamia Baidouri");
+                Debug.Log("Rocket animated");
+            }
+
+            if (checkpoint.checkpointID.Equals("Car"))
+            {
+                checkpointCanvas.enabled = true;
+                uiManager.UpdateText(
+                    "Cars and races, a fusion of adrenaline and engineering mastery, epitomize the pinnacle of speed and skill. They ignite our senses, propelling us into a world where split-second decisions define victory or defeat.\nThe Group");
+                raceButton.gameObject.SetActive(true);
             }
             
        }
