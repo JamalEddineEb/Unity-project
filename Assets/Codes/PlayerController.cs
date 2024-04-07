60d524e06d82923966703bcb69098b50b084891a
JamalEddineEb <jamal-eddine.el-betioui@etu.unice.fr>
2024-04-07 15:16:46 +0200

    animate rocket

--- Assets/Codes/PlayerController.cs
+++ Assets/Codes/PlayerController.cs
@@ -25,16 +25,17 @@ public class PlayerController : MonoBehaviour
 
         // Calculate movement
         Vector3 movement = this.transform.forward * verticalAxis + this.transform.right * horizontalAxis;
-        movement.Normalize();
+        rb.MovePosition(rb.position + movement * 0.4f);
 
         // Move the player
-        this.transform.position += movement * 0.4f;
+        this.transform.position += movement * 0.2f;
 
         // Rotate the player based on horizontal input
         if (horizontalAxis != 0)
         {
-            float rotationAmount = horizontalAxis * 100f * Time.deltaTime;
-            this.transform.Rotate(Vector3.up, rotationAmount);
+            float rotationAmount = horizontalAxis * 50f * Time.fixedDeltaTime;
+            Quaternion deltaRotation = Quaternion.Euler(Vector3.up * rotationAmount);
+            rb.MoveRotation(rb.rotation * deltaRotation);
         }
 
         // Set animator parameters
