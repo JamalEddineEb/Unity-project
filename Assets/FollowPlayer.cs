32e0731c4209fd3f3a7c90ae1b2b02678e664eb7
JamalEddineEb <jamal-eddine.el-betioui@etu.unice.fr>
2024-04-09 18:37:28 +0200

    2 scenes

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
-    public Vector3 marginFromPlayer;
+    public Vector3 offset; // Offset from the player's position
 
-    void Update()
+    void LateUpdate()
     {
-        transform.position = player.transform.position + marginFromPlayer;
+        // Calculate the desired position behind the player
+        Vector3 targetPosition = player.position - player.forward * offset.z + player.up * offset.y;
+
+        // Set the position of the GameObject
+        transform.position = targetPosition;
+
+        // Make the GameObject look at the player
+        transform.LookAt(player);
     }
 }
